fa4d56633161509894bc2227517b56258b6e2f0a
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-06-16 09:19:23 +0530

    Update app.py

--- local-pipeline-demo/src/app.py
+++ local-pipeline-demo/src/app.py
@@ -1,1 +1,9 @@
-print("Hello from the local pipeline demo app!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from the local pipeline demo app!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
