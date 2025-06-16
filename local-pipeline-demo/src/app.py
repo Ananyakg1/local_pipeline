7e456264dfad0745a834919382857b6dc6f8ac90
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-06-16 10:11:12 +0530

    Update app.py

--- local-pipeline-demo/src/app.py
+++ local-pipeline-demo/src/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 
 @app.route('/')
 def home():
-    return "Hello from the local pipeline demo app!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
